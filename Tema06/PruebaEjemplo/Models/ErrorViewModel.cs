1e69fc0cba5127d4577c11ae10547b569da99bb6
mreqmer <marta.requejo@iesnervion.es>
2024-10-24 08:58:30 +0200

    resubida

---
+++ Tema06/PruebaEjemplo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaEjemplo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
