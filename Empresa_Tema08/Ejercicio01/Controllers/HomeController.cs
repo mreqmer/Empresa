ced5003044a7e66a8417b89c991ff60282252301
mreqmer <marta.requejo@iesnervion.es>
2024-10-29 14:35:47 +0100

    *-

---
+++ Empresa_Tema08/Ejercicio01/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Ejercicio01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index(string nombre)
+        {
+            return View();
+        }
+        public IActionResult Saludo(String nombre)
+        {
+            ViewBag.Nombre = nombre;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Empresa_Tema08/Ejercicio02/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using Ejercicio02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string nombre)
+        {
+            ViewBag.Nombre = nombre;
+            return View("Saludo");
+        }
+
+        public ActionResult Saludo()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
